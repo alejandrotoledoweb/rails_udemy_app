b71537db4abedb8a9e907d94b5b0ce7b6faeeb8e
Alejandro Toledo <atoledofr@gmail.com>
2024-02-14 12:06:59 -0500

    give link classes

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -12,14 +12,14 @@ module CoursesHelper
   def enrollment_button(course)
     if current_user
       if course.user === current_user
-         link_to "See Analytics", course_path(course), class: "rounded-lg py-1 mb-2 px-3 bg-lime-100 font-medium mr-2 hover:bg-lime-300"
+         link_to "See Analytics", course_path(course), class: ""
       elsif course.enrollments.where(user: current_user).any?
-        link_to "Continue", course_path(course), class: "rounded-lg py-1 mb-2 px-3 bg-lime-100 font-medium mr-2 hover:bg-lime-300"
+        link_to "Continue", course_path(course), class: ""
       elsif course.price > 0
-        link_to "Buy this course", new_course_enrollment_path(course), class: "rounded-lg py-1 mb-2 px-3 bg-lime-100 font-medium mr-2 hover:bg-lime-300"
+        link_to "Buy this course", new_course_enrollment_path(course), class: ""
 
       else
-        link_to "Free", new_course_enrollment_path(course), class: "rounded-lg py-1 mb-2 px-3 bg-lime-100 font-medium mr-2 hover:bg-lime-300"
+        link_to "Free", new_course_enrollment_path(course), class: ""
 
       end
       #logic to buy
