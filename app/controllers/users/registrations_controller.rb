2dedf588f2abbdb9be0fa504e8bed0f5af181107
Alejandro Toledo <atoledofr@gmail.com>
2024-03-28 11:50:05 -0500

    fix messages color bg and text

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'pry'
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -103,11 +104,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # Overriding the method to not require current password for OAuth users
   def update_resource(resource, params)
-    if resource.provider? # Check if the user signed up via OAuth
+    binding.pry
+    unless params[:password].present? && params[:password_confirmation].present?
       resource.update_without_password(params.except("current_password"))
     else
       super
     end
+    # if resource.provider? # Check if the user signed up via OAuth
+    # else
+      # super
+    # end
   end
 
   private
