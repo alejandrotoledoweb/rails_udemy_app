427ec168c02a61cd8900fb66961266463b5d6689
Alejandro Toledo <atoledofr@gmail.com>
2024-04-05 11:51:21 -0500

    add validation for user with provider email

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,10 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
-    user.provider = "yes"
-    user.save!
+    if user
+      user.provider = "yes"
+      user.save!
+    end
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
